87aae4f205545ca07a39cc4f8b129c10a70bad09
Reid W <westwood.reid@gmail.com>
2026-08-18 17:43:40 -0700

    test: bind sdist-wheel runner artifact

--- tests/test_dependency_provenance.py
+++ tests/test_dependency_provenance.py
@@ -30,7 +30,7 @@ VERSIONS = {
 }
 WHEEL_HASHES = {
     "agentic": "9754e9eb9dd788b4276a7cb3cb1a26638d52fd193a38abae2c688fba7d16fce3",
-    "codegen": "26856622891d1f25b205d91d3c3a75706bddadb39a91c070728e59616d639743",
+    "codegen": "8a16324caa92ed22403aab9c9599b70f02fad3a6fc11d4b801a28f8d0b3d6180",
     "costingfe": "970ed533d8fae042de25256933ec99d3385092903e4d407ab2b96baa7a2fcfd6",
 }
 
